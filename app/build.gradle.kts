8712629d2572d7db07cbcfd8938e438f96596d7a
wicahma <teguh.dc.kusuma@gmail.com>
2024-03-07 11:24:07 +0700

    update: some data and access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-parcelize")
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 android {
@@ -41,8 +43,12 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    //noinspection UseTomlInstead
+    implementation("com.github.bumptech.glide:glide:4.15.0")
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
