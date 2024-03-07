9c93f88033c7a602b2d01e0c1abb06855c35aa24
wicahma <teguh.dc.kusuma@gmail.com>
2024-03-07 11:21:43 +0700

    update: adding author activity page

---
+++ app/src/main/java/com/basic/foodrecipessubmissionapp/AboutActivity.kt
@@ -0,0 +1,22 @@
+package com.basic.foodrecipessubmissionapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setTitle("About")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
